83acbca0c95e1e8ed704581b0d847dcb630a5450
Israel Lagos Rocha <israelandreslagosrocha@gmail.com>
2026-05-15 18:17:51 -0400

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/HARMONIGRID/',
+
   plugins: [
     vue(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
